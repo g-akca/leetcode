77719a79779faafa293d3c00298cf9ae6a35eeaa
g-akca <gok.akca66@gmail.com>
2025-05-17 01:05:49 +0300

    [Easy] 1893. Check if All the Integers in a Range Are Covered - C++

---
+++ Easy/1893. Check if All the Integers in a Range Are Covered/Solution.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    bool isCovered(vector<vector<int>>& ranges, int left, int right) {
+        for (int i = left; i <= right; i++) {
+            bool found = false;
+            for (int j = 0; j < ranges.size(); j++) {
+                if (i >= ranges[j][0] && i <= ranges[j][1]) {
+                    found = true;
+                }
+            }
+            if (!found) return false;
+        }
+        return true;
+    }
+};
