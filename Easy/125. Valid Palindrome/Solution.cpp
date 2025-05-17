89f0b71f83a87af4ad6d4e4bde929be91b327c93
g-akca <gok.akca66@gmail.com>
2025-05-18 00:10:26 +0300

    [Easy] 125. Valid Palindrome - C++

---
+++ Easy/125. Valid Palindrome/Solution.cpp
@@ -0,0 +1,24 @@
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int i = 0, j = s.length() - 1;
+        while (j > i) {
+            if (!isalnum(s[i])) {
+                i++;
+                continue;
+            }
+            if (!isalnum(s[j])) {
+                j--;
+                continue;
+            }
+            
+            if (tolower(s[i]) != tolower(s[j])) return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+};
