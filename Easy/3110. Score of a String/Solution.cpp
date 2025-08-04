37d1a62bfe2606cc3a7db1e9358d9eab0a9bdd43
g-akca <gok.akca66@gmail.com>
2025-08-04 17:10:48 +0300

    [Easy] 3110. Score of a String - C++

---
+++ Easy/3110. Score of a String/Solution.cpp
@@ -0,0 +1,14 @@
+#include <string>
+
+class Solution {
+public:
+    int scoreOfString(string s) {
+        int score = 0;
+
+        for (int i = 0; i < s.length() - 1; i++) {
+            score += abs(s[i + 1] - s[i]);
+        }
+
+        return score;
+    }
+};
