00ac1746b559aeaddaf492f0db9b0c573b689638
g-akca <gok.akca66@gmail.com>
2025-08-05 16:27:34 +0300

    [Easy] 2255. Count Prefixes of a Given String - C++

---
+++ Easy/2255. Count Prefixes of a Given String/Solution.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int countPrefixes(vector<string>& words, string s) {
+        int count = 0;
+        for (int i = 0; i < words.size(); i++) {
+            if (s.find(words[i]) == 0) count++;
+        }
+        return count;
+    }
+};
